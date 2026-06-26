b0aa4ac7e62c6269f1cf332cf95769c0d7db9597
ivanq0 <vaneksizov2018@gmail.com>
2026-06-26 12:32:03 +0600

    feat: add solution for task01 and add CI workflow

---
+++ practice2026/task01/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace task01
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string input)
+        {
+            string input_tolower = input.ToLower();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in input_tolower)
+            {
+                if (!(char.IsPunctuation(c) || char.IsWhiteSpace(c))) sb.Append(c); 
+            }
+
+            string clear_input = sb.ToString();
+
+            if (clear_input.Length == 0) return false;
+
+            for (int i = 0; i < clear_input.Length; i++)
+            {
+                if (clear_input[i] != clear_input[^(i + 1)]) return false;
+            }
+            return true;
+        }
+    }
+}
