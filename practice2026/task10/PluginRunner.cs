abbb60061dc70c0ba4230223672887298997855a
ivanq0 <vaneksizov2018@gmail.com>
2026-07-06 15:51:59 +0600

    feat: add plugin system and tests

---
+++ practice2026/task10/PluginRunner.cs
@@ -0,0 +1,131 @@
+﻿using System.Reflection;
+using task07;
+
+namespace task10;
+
+public class PluginRunner
+{
+    static void Main()
+    {
+        string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        try
+        {
+            List<Type> classesTypes = SearchDll(baseDirectory);
+            Dictionary<Type, List<Type>> dependencyGraph = GetDependencyGraph(classesTypes);
+            List<Type> sortedPlugins = SortPlugins(dependencyGraph);
+            RunPlugins(sortedPlugins);
+        }
+        catch (Exception ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"[Ошибка системы]: {ex.Message}");
+            Console.ResetColor();
+        }
+    }
+
+    static List<Type> SearchDll(string directoryPath)
+    {
+        var classesTypes = new List<Type>();
+
+        if (!Directory.Exists(directoryPath)) return classesTypes;
+
+        string[] dllFiles = Directory.GetFiles(directoryPath, "*.dll");
+        foreach (var dll in dllFiles)
+        {
+            try
+            {
+                Assembly assembly = Assembly.LoadFrom(dll);
+                foreach (Type type in assembly.GetTypes())
+                {
+                    if (type.IsClass && !type.IsAbstract && Attribute.IsDefined(type, typeof(PluginLoadAttribute)))
+                    {
+                        classesTypes.Add(type);
+                    }
+                }
+            }
+            catch { }
+        }
+        return classesTypes;
+    }
+
+    public static Dictionary<Type, List<Type>> GetDependencyGraph(List<Type> classesTypes)
+    {
+        var dependencyGraph = new Dictionary<Type, List<Type>>();
+
+        foreach (var classType in classesTypes)
+        {
+            dependencyGraph[classType] = new List<Type>();
+
+            var attribute = classType.GetCustomAttribute<PluginLoadAttribute>();
+            if (attribute?.Dependencies != null)
+            {
+                foreach (var depName in attribute.Dependencies)
+                {
+                    var dependencyType = classesTypes.Find(p => p.Name == depName);
+                    if (dependencyType != null)
+                    {
+                        dependencyGraph[classType].Add(dependencyType);
+                    }
+                    else
+                    {
+                        throw new Exception($"Плагин {classType.Name} требует зависимость '{depName}', но она не найдена в папке.");
+                    }
+                }
+            }
+        }
+        return dependencyGraph;
+    }
+
+    public static List<Type> SortPlugins(Dictionary<Type, List<Type>> dependencyGraph)
+    {
+        var sortedPlugins = new List<Type>();
+        var visited = new Dictionary<Type, bool>();
+
+        foreach (var plugin in dependencyGraph.Keys)
+        {
+            Visit(plugin, dependencyGraph, visited, sortedPlugins);
+        }
+        return sortedPlugins;
+    }
+
+    static void Visit(Type plugin, Dictionary<Type, List<Type>> dependencyGraph, Dictionary<Type, bool> visited, List<Type> sortedPlugins)
+    {
+        if (visited.TryGetValue(plugin, out bool isFullyVisited))
+        {
+            if (!isFullyVisited)
+                throw new Exception($"Обнаружена циклическая зависимость! Компонент '{plugin.Name}' зациклен сам на себя.");
+            return;
+        }
+
+        visited[plugin] = false;
+
+        foreach (var dependency in dependencyGraph[plugin])
+        {
+            Visit(dependency, dependencyGraph, visited, sortedPlugins);
+        }
+
+        visited[plugin] = true;
+        sortedPlugins.Add(plugin);
+    }
+
+    static void RunPlugins(List<Type> sortedPlugins)
+    {
+        Console.WriteLine("\n--- Запуск модулей системы ---");
+        foreach (var type in sortedPlugins)
+        {
+            Console.WriteLine($"[Система]: Запуск {type.Name}...");
+
+            var instance = Activator.CreateInstance(type);
+            var method = type.GetMethod("Execute", BindingFlags.Public | BindingFlags.Instance);
+
+            if (method != null)
+            {
+                method.Invoke(instance, null);
+            }
+            else
+            {
+                throw new Exception($"У плагина {type.Name} отсутствует обязательный метод 'Execute()'.");
+            }
+        }
+    }
+}
---
+++ practice2026/task10tests/PluginRunnerTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using task07;
+using task10;
+
+namespace task10tests;
+
+public class PluginRunnerTests
+{
+    [PluginLoad]
+    private class PluginA { }
+    [PluginLoad("PluginA")]
+    private class PluginB { }
+    [PluginLoad("PluginB")]
+    private class PluginC { }
+
+    [PluginLoad("PluginY")]
+    private class PluginX { }
+    [PluginLoad("PluginX")]
+    private class PluginY { }
+
+    [PluginLoad("NonExistentPlugin")]
+    private class BadPlugin { }
+
+    [Fact]
+    public void SortPlugins_ShouldCorrectSequence_ForValidDependencies()
+    {
+        var plugins = new List<Type> { typeof(PluginB), typeof(PluginA), typeof(PluginC) };
+
+        var graph_dependencies = PluginRunner.GetDependencyGraph(plugins);
+        var sorted_plugins = PluginRunner.SortPlugins(graph_dependencies);
+
+        Assert.Equal(3, sorted_plugins.Count);
+        int indexA = sorted_plugins.IndexOf(typeof(PluginA));
+        int indexB = sorted_plugins.IndexOf(typeof(PluginB));
+        int indexC = sorted_plugins.IndexOf(typeof(PluginC));
+        Assert.True(indexA < indexB);
+        Assert.True(indexB < indexC);
+    }
+
+    [Fact]
+    public void GetDependencyGraph_ShouldThrowException_ForNoExistentDependency()
+    {
+        var plugins = new List<Type> { typeof(BadPlugin) };
+
+        try
+        {
+            PluginRunner.GetDependencyGraph(plugins);
+            Assert.Fail("Метод должен был выбросить ошибку из-за отсутствия зависимости");
+        }
+        catch (Exception ex) { Assert.Contains("она не найдена", ex.Message); }
+    }
+
+    [Fact]
+    public void SortPlugins_ShouldThrowException_ForСyclicDependency()
+    {
+        var plugins = new List<Type> { typeof(PluginY), typeof(PluginX) };
+        var graph_dependencies = PluginRunner.GetDependencyGraph(plugins);
+
+        try
+        {
+            PluginRunner.SortPlugins(graph_dependencies);
+            Assert.Fail("Метод должен был выбросить ошибку из-за циклической зависимости");
+        }
+        catch (Exception ex) { Assert.Contains("Обнаружена циклическая зависимость!", ex.Message); }
+    }
+}
