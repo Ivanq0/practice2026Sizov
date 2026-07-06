38ddb6002fea77e605fe8c72b4ea71821457f249
ivanq0 <vaneksizov2018@gmail.com>
2026-07-06 14:09:53 +0600

    fix: GlobalUsings.cs

--- practice2026/task04tests/GlobalUsings.cs
+++ practice2026/task04tests/GlobalUsings.cs
@@ -1,12 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace task04tests
-{
-    internal class GlobalUsings
-    {
-    }
-}
+﻿global using Xunit;
